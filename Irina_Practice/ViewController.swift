5179c6bcbfb7e65178c3d66f956f5889421f8992
IrinaRaleigh <63532291+IrinaRaleigh@users.noreply.github.com>
2021-06-29 13:24:58 -0500

    Initial Commit

---
+++ Irina_Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Irina_Practice
+//
+//  Created by Irina Raleigh on 6/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
